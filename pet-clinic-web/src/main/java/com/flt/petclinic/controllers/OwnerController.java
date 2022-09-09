7f02c38afd2789b4cdcf8a0e4299f026c317e194
nipunaupeksha <nipuna.upeksha@gmail.com>
2022-09-10 02:13:40 +0530

    refactor vet and owner controllers.

--- pet-clinic-web/src/main/java/com/flt/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/flt/petclinic/controllers/OwnerController.java
@@ -3,10 +3,11 @@ package com.flt.petclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    @RequestMapping({"","/", "/index", "/index.html"})
     public String index() {
 
         return "owners/index";
--- pet-clinic-web/src/main/java/com/flt/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/flt/petclinic/controllers/VetController.java
@@ -3,10 +3,11 @@ package com.flt.petclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"", "/", " /index", " /index.html"})
     public String index() {
 
         return "vets/index";
