383cab9bb89e46e84a6d75094a0eadca3c2475f2
nipunaupeksha <nipuna.upeksha@gmail.com>
2022-09-11 16:25:34 +0530

    Refactor Map Impl to ID generation

--- pet-clinic-web/src/main/java/com/flt/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/flt/petclinic/bootstrap/DataLoader.java
@@ -23,14 +23,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Harry");
         owner1.setLastName("Potter");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Ron");
         owner2.setLastName("Weasley");
 
@@ -39,14 +37,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Hermione");
         vet1.setLastName("Granger");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Fred");
         vet2.setLastName("Weasley");
 
