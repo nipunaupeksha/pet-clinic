15de098fd2b5a9a4af7cd04211435e6e556f7202
nipunaupeksha <nipuna.upeksha@gmail.com>
2022-09-11 07:58:12 +0530

    List Owners

--- pet-clinic-web/src/main/java/com/flt/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/flt/petclinic/controllers/OwnerController.java
@@ -1,15 +1,30 @@
 package com.flt.petclinic.controllers;
 
+import com.flt.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"","/", "/index", "/index.html"})
-    public String index() {
+    private final OwnerService ownerService;
 
+    public OwnerController(OwnerService ownerService) {
+
+        this.ownerService = ownerService;
+    }
+
+    /**
+     * List owners in the index page.
+     * @param model Model object.
+     * @return The index page listing the owners.
+     */
+    @RequestMapping({"","/", "/index", "/index.html"})
+    public String index(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
