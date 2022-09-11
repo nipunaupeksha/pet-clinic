9eb6b24bf6e347dd5caaeb1abc758b631065518a
nipunaupeksha <nipuna.upeksha@gmail.com>
2022-09-11 14:41:15 +0530

    List Vets

--- pet-clinic-web/src/main/java/com/flt/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/flt/petclinic/controllers/VetController.java
@@ -1,15 +1,25 @@
 package com.flt.petclinic.controllers;
 
+import com.flt.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({"", "/", " /index", " /index.html"})
-    public String index() {
+    private final VetService vetService;
 
+    public VetController(VetService vetService) {
+
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/", " /index", " /index.html"})
+    public String index(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
